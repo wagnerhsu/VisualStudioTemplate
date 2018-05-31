7a9f78c07b856992cbabdc5ea96c983708600a53
Wagner <wagnerhsu@hotmail.com>
2018-05-31 14:29:45 +0800

    Add template projects based on .NET Core 2.0 and 2.1

---
+++ DotnetCore-2.1/MvcAspNetWeb/MvcAspNetWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcAspNetWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
