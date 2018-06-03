ded67487b3456aa9fee9e989eda3886cc71ac802
Wagner <wagnerhsu@hotmail.com>
2018-06-03 20:00:42 +0800

    MVC with Individual Authentication

---
+++ DotnetCore-2.1/MvcAspNetWeb-Individual/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcAspNetWeb_Individual.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
