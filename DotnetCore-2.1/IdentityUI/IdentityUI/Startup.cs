9d33ac841359a5ee628169d0915e941287e4697a
Wagner <wagnerhsu@hotmail.com>
2018-09-05 13:55:42 +0800

    Add more options to identity

--- DotnetCore-2.1/IdentityUI/IdentityUI/Startup.cs
+++ DotnetCore-2.1/IdentityUI/IdentityUI/Startup.cs
@@ -37,7 +37,16 @@ namespace IdentityUI
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
+            services.AddDefaultIdentity<IdentityUser>(opts =>
+            {
+                // Add more options
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            })
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
