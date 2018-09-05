fbbffb1d11451d62277af5e9bea8a6c8f6f49d88
Wagner <wagnerhsu@hotmail.com>
2018-09-05 13:41:17 +0800

    IdentityUI

---
+++ DotnetCore-2.1/IdentityUI/IdentityUI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IdentityUI.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityUI.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityUI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
