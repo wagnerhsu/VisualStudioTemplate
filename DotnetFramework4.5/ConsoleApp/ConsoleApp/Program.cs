53d89a92ba605d8e06308f7bf3bcdabc9091b379
Wagner <wagnerhsu@hotmail.com>
2018-10-17 15:09:34 +0800

    Update Microsoft.AspNetCore.All to 2.1.5

--- DotnetFramework4.5/ConsoleApp/ConsoleApp/Program.cs
+++ DotnetFramework4.5/ConsoleApp/ConsoleApp/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MoreLinq.Extensions;
+using System;
 
 namespace ConsoleApp
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            byte[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            Span<byte> span1 = new Span<byte>(arr);
+            var span2 = span1.Slice(1, 2);
+            span2[0] = 10;
+            arr.ForEach(x =>
+            {
+                Console.Write(x);
+                Console.Write("\t");
+            });
         }
     }
 }
